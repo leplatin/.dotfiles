abdd1538a6dd195429bd0fc5f61c4d3e303fa45f
mnkey <helllbilly@gmail.com>
2023-03-31 13:37:26 +0300

    clean nvim duplicate folder

--- .config/nvim/lua/mnkey/core/colorscheme.lua
+++
@@ -1,8 +0,0 @@
---vim.cmd("colorscheme nord")
--- set colorscheme to nord with protected call in case it isn't installed
-
-local status, _ = pcall(vim.cmd, "colorscheme nord")
-if not status then
-  print("Colorscheme not found!") -- print error if colorscheme not installed
-  return
-end
